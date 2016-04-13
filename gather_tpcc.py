1e7b0af03c89b3004664d60c54524be5fc292f8d
Xian Ran <xranthoar@gmail.com>
2016-04-12 23:40:26 -0400

    add abort rate

--- gather_tpcc.py
+++ gather_tpcc.py
@@ -8,7 +8,16 @@ def get_throughput(dir_name, file_name):
     result = re.search(r' (\d+\.\d+) requests/sec', content)
     return float(result.group(1))
 
-print ",".join(["Protocol", "Scale", "NewOrder", "Payment", "RPS"])
+def get_abort_rate(dir_name):
+    if not os.path.exists("%s/peloton.log" % (dir_name)):
+        return 0.0
+    f = open("%s/peloton.log" % (dir_name), "r")
+    content = f.read()
+    f.close()
+    result = re.search(r'TxnTotal = (\d+), TxnSuccess = (\d+)', content)
+    return (float(result.group(1))-float(result.group(2))) / float(result.group(1))
+
+print ",".join(["Protocol", "Scale", "NewOrder", "Payment", "RPS", "AbrtRate"])
 for protocol in ["OPTIMISTIC", "PESSIMISTIC", "SSI", "TO", "SPECULATIVE_READ", "EAGER_WRITE"]:
     for scale in [1, 4, 8, 12]:
         dir_name = "tpcc_collected_data_%s_%d_%d_%d_%d_%d_%d" % (protocol, 100, 0, 0, 0, 0, scale)
@@ -16,4 +25,5 @@ for protocol in ["OPTIMISTIC", "PESSIMISTIC", "SSI", "TO", "SPECULATIVE_READ", "
 
         if os.path.exists("%s/%s" % (dir_name, file_name)):
             rps = get_throughput(dir_name, file_name)
-            print ','.join([protocol, str(scale), "100", "0", str(rps)])
+            abt_rate = get_abort_rate(dir_name)
+            print ','.join([protocol, str(scale), "100", "0", str(rps), "%.3f" % (abt_rate * 100)])
--- gather_ycsb.py
+++ gather_ycsb.py
@@ -8,7 +8,16 @@ def get_throughput(dir_name, file_name):
     result = re.search(r' (\d+\.\d+) requests/sec', content)
     return float(result.group(1))
 
-print ",".join(["Protocol", "Contention", "Read", "Write", "MR", "RPS"])
+def get_abort_rate(dir_name):
+    if not os.path.exists("%s/peloton.log" % (dir_name)):
+        return 0.0
+    f = open("%s/peloton.log" % (dir_name), "r")
+    content = f.read()
+    f.close()
+    result = re.search(r'TxnTotal = (\d+), TxnSuccess = (\d+)', content)
+    return (float(result.group(1))-float(result.group(2))) / float(result.group(1))
+
+print ",".join(["Protocol", "Contention", "Read", "Write", "MR", "RPS", "AbrtRate"])
 for protocol in ["OPTIMISTIC", "PESSIMISTIC", "SSI", "TO", "SPECULATIVE_READ", "EAGER_WRITE"]:
     for contention in ["0.00", "0.10", "0.20", "0.30", "0.40", "0.50", "0.60", "0.70", "0.80", "0.90", "0.99"]:
         for read_ratio in ["0", "30", "50", "70", "100"]:
@@ -24,4 +33,5 @@ for protocol in ["OPTIMISTIC", "PESSIMISTIC", "SSI", "TO", "SPECULATIVE_READ", "
         if os.path.exists("%s/%s" % (dir_name, file_name)):
 
             rps = get_throughput(dir_name, file_name)
-            print ','.join([protocol, contention, "0", "0", "100", str(rps)])
+            abt_rate = get_abort_rate(dir_name)
+            print ','.join([protocol, contention, "0", "0", "100", str(rps), "%.3f" % (abt_rate * 100)])
