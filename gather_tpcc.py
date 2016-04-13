14aa9c502a1b52abd6579442503f7adb766771e1
Xian Ran <xranthoar@gmail.com>
2016-04-12 21:32:01 -0400

    gather avg thput

--- gather_tpcc.py
+++ gather_tpcc.py
@@ -1,16 +1,19 @@
 import os
 
+
+def get_throughput(dir_name, file_name):
+    f = open("%s/%s" % (dir_name, file_name), "r")
+    content = f.read()
+    f.close()
+    result = re.search(r' (\d+\.\d+) requests/sec', content)
+    return float(result.group(1))
+
 print ",".join(["Protocol", "Scale", "NewOrder", "Payment", "RPS"])
 for protocol in ["OPTIMISTIC", "PESSIMISTIC", "SSI", "TO", "SPECULATIVE_READ", "EAGER_WRITE"]:
     for scale in [1, 4, 8, 12]:
         dir_name = "tpcc_collected_data_%s_%d_%d_%d_%d_%d_%d" % (protocol, 100, 0, 0, 0, 0, scale)
-        file_name = "outputfile_%s_%d_%d_%d_%d_%d_%d.res" % (protocol, 100, 0, 0, 0, 0, scale)
+        file_name = "outputfile_%s_%d_%d_%d_%d_%d_%d.log" % (protocol, 100, 0, 0, 0, 0, scale)
 
         if os.path.exists("%s/%s" % (dir_name, file_name)):
-            f = open("%s/%s" % (dir_name, file_name), "r")
-            content = f.readline()
-            content = f.readline()
-            f.close()
-
-            rps = float(content.split(",")[1])
+            rps = get_throughput(dir_name, file_name)
             print ','.join([protocol, str(scale), "100", "0", str(rps)])
--- gather_ycsb.py
+++ gather_ycsb.py
@@ -1,30 +1,27 @@
 import os
+import re
+
+def get_throughput(dir_name, file_name):
+    f = open("%s/%s" % (dir_name, file_name), "r")
+    content = f.read()
+    f.close()
+    result = re.search(r' (\d+\.\d+) requests/sec', content)
+    return float(result.group(1))
 
 print ",".join(["Protocol", "Contention", "Read", "Write", "MR", "RPS"])
-for protocol in ["OPTIMISTIC", "PESSIMISTIC", "SSI", "TO", "SPECULATIVE_READ", "EAGER_WRITE"]:
+for protocol in ["OPTIMISTIC"]:
     for contention in ["0.00", "0.10", "0.20", "0.30", "0.40", "0.50", "0.60", "0.70", "0.80", "0.90", "0.99"]:
         for read_ratio in ["0", "30", "50", "70", "100"]:
-            dir_name = "ycsb_collected_data_%s_%s_%s_%s_%s_%d" % (protocol, contention, read_ratio, "0", str(100-int(read_ratio)), 20)
-            file_name = "outputfile_%s_%s_%s_%s_%s_%d.res" % (protocol, contention, read_ratio, "0", str(100 - int(read_ratio)), 20)
-
+            dir_name = "ycsb_collected_data_%s_%s_%s_%s_%s_%s" % (protocol, contention, read_ratio, "0", str(100-int(read_ratio)), "0")
+            file_name = "outputfile_%s_%s_%s_%s_%s_%s.log" % (protocol, contention, read_ratio, "0", str(100 - int(read_ratio)), "0")
             if os.path.exists("%s/%s" % (dir_name, file_name)):
-                f = open("%s/%s" % (dir_name, file_name), "r")
-                content = f.readline()
-                content = f.readline()
-                f.close()
-
-                rps = float(content.split(",")[1])
+                rps = get_throughput(dir_name, file_name)
                 print ','.join([protocol, contention, read_ratio, str(100-int(read_ratio)), "0", str(rps)])
 
-
         dir_name = "ycsb_collected_data_%s_%s_0_0_0_100" % (protocol, contention)
-        file_name = "outputfile_%s_%s_0_0_0_100.res" % (protocol, contention)
+        file_name = "outputfile_%s_%s_0_0_0_100.log" % (protocol, contention)
 
         if os.path.exists("%s/%s" % (dir_name, file_name)):
-            f = open("%s/%s" % (dir_name, file_name), "r")
-            content = f.readline()
-            content = f.readline()
-            f.close()
 
-            rps = float(content.split(",")[1])
+            rps = get_throughput(dir_name, file_name)
             print ','.join([protocol, contention, "0", "0", "100", str(rps)])
